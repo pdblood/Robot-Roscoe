6083e3ea781c0b3dcbbf82dba083608b91a62eac
Philip Blood <pdblood@gmail.com>
2019-06-26 14:26:40 +0000

    Redo of changes to motor control - this time using correct commands for Arduino Due

--- Control.ino
+++ Control.ino
@@ -4,6 +4,7 @@
 // Control functions (PID controls, Steppers control...)
 
 // PD controller implementation(Proportional, derivative). DT in seconds
+//PDB: Changing Timer logic to use DueTimer Library
 float stabilityPDControl(float DT, float input, float setPoint,  float Kp, float Kd)
 {
   float error;
@@ -51,35 +52,53 @@ float positionPDControl(long actualPos, long setPointPos, float Kpp, float Kdp, 
   return (output);
 }
 
+void stepDelay() {
+	// Code below should be about 2 µS on a 84 Mhz Arduino Due
+	for (auto i = 0; i < 40; ++i) {
+		asm("nop \n");
+	}
+}
+
 //PDB: Need to change this to a Due Timer
-// TIMER 1 : STEPPER MOTOR1 SPEED CONTROL
-ISR(TIMER1_COMPA_vect)
+//PDB Callback for TIMER 6 : STEPPER MOTOR1 SPEED CONTROL
+void runMotor1()
 {
   if (dir_M1 == 0) // If we are not moving we dont generate a pulse
     return;
-  // We generate 1us STEP pulse
-  SET(PORTB, 25); // STEP MOTOR 1
-  //delay_1us();
+
+  // following this method:
+  // https://hackaday.io/project/16483-building-the-thor-robot/log/59588-motor-control-faster-and-faster
+
+  //Digital Pin 2 (Controlling X Motor Step) set HIGH - Start Pulse
+  REG_PIOB_SODR = PIO_PB25; 
+  // 2 µS pulse on a 84 Mhz Arduino Due
+  stepDelay();
+
   if (dir_M1 > 0)
     steps1--;
   else
     steps1++;
-  CLR(PORTB, 25);
+
+  //Digital Pin 2 Set Low - End Pulse
+  REG_PIOB_CODR = PIO_PB25; 
 }
 //PDB: Change to a Due Timer
-// TIMER 3 : STEPPER MOTOR2 SPEED CONTROL
-ISR(TIMER3_COMPA_vect)
+//PDB: Callback for TIMER 7 : STEPPER MOTOR2 SPEED CONTROL
+void runMotor2()
 {
   if (dir_M2 == 0) // If we are not moving we dont generate a pulse
     return;
-  // We generate 1us STEP pulse
-  SET(PORTC, 28); // STEP MOTOR 2
-  //delay_1us();
+
+  REG_PIOC_SODR = PIO_PC28; //Pin 3 (Controlling Y Motor Step) - start pulse
+  // 2 µS pulse on a 84 Mhz Arduino Due
+  stepDelay();
+
   if (dir_M2 > 0)
     steps2--;
   else
     steps2++;
-  CLR(PORTC, 28);
+
+  REG_PIOC_CODR = PIO_PC28; //Pin 3 Step end pulse
 }
 
 
@@ -87,8 +106,8 @@ ISR(TIMER3_COMPA_vect)
 // tspeed could be positive or negative (reverse)
 void setMotorSpeedM1(int16_t tspeed)
 {
-  long timer_period;
-  int16_t speed;
+  double timer_period;
+  double speed;
 
   // Limit max speed?
 
@@ -100,12 +119,22 @@ void setMotorSpeedM1(int16_t tspeed)
   else
     speed_M1 = tspeed;
 
+#if DEBUG > 0
+  Serial.print("tspeed1: ");
+  Serial.println(tspeed);
+#endif
+
 #if MICROSTEPPING==32
   speed = speed_M1 * 100; // Adjust factor from control output speed to real motor speed in steps/second
 #elif MICROSTEPPING==16
   speed = speed_M1 * 50; // Adjust factor from control output speed to real motor speed in steps/second
 #else
   speed = speed_M1 * 25; // 1/8 Microstepping
+#endif
+
+#if DEBUG > 0
+  Serial.print("speed1: ");
+  Serial.println(speed);
 #endif
 
   if (speed == 0)
@@ -115,31 +144,33 @@ void setMotorSpeedM1(int16_t tspeed)
   }
   else if (speed > 0)
   {
-    timer_period = 2000000 / speed; // 2Mhz timer //PDB change for Due
+    timer_period = 1000000.0l / speed; //PDB sec/step * 1,000,000 =  useconds/step
     dir_M1 = 1;
-    SET(PORTC, 25); // DIR Motor 1 (Forward)
+    REG_PIOC_SODR = PIO_PC25; //Pin 5 (X Motor Dir) Set High
   }
   else
   {
-    timer_period = 2000000 / -speed; //PDB change for Due
+    timer_period = 1000000.0l / -speed; //PDB sec/step * 1,000,000 =  useconds/step
     dir_M1 = -1;
-    CLR(PORTC, 25); // Dir Motor 1
+    REG_PIOC_CODR = PIO_PC25; //Pin 5 Set Low (opposite direction)
   }
-  if (timer_period > 65535)   // Check for minimun speed (maximun period without overflow)
+  if (timer_period > ZERO_SPEED)  // Check that timer does not exceed max period
     timer_period = ZERO_SPEED;
 
-  OCR1A = timer_period;
-  // Check  if we need to reset the timer...
-  if (TCNT1 > OCR1A)
-    TCNT1 = 0;
+#if DEBUG > 0
+  Serial.print("timer6_period_control: ");
+  Serial.println(timer_period);
+#endif
+
+  Timer6.setPeriod(timer_period).start();
 }
 
 // Set speed of Stepper Motor2
 // tspeed could be positive or negative (reverse)
 void setMotorSpeedM2(int16_t tspeed)
 {
-  long timer_period;
-  int16_t speed;
+  double timer_period;
+  double speed;
 
   // Limit max speed?
 
@@ -151,12 +182,22 @@ void setMotorSpeedM2(int16_t tspeed)
   else
     speed_M2 = tspeed;
 
+#if DEBUG > 0
+  Serial.print("tspeed2: ");
+  Serial.println(speed);
+#endif
+
 #if MICROSTEPPING==32
   speed = speed_M2 * 100; // Adjust factor from control output speed to real motor speed in steps/second
 #elif MICROSTEPPING==16
   speed = speed_M2 * 50; // Adjust factor from control output speed to real motor speed in steps/second
 #else
   speed = speed_M2 * 25; // 1/8 Microstepping
+#endif
+
+#if DEBUG > 0
+  Serial.print("speed2: ");
+  Serial.println(speed);
 #endif
 
   if (speed == 0)
@@ -166,22 +207,24 @@ void setMotorSpeedM2(int16_t tspeed)
   }
   else if (speed > 0)
   {
-    timer_period = 2000000 / speed; // 2Mhz timer
+    timer_period = 1000000.0l / speed; //PDB sec/step * 1,000,000 =  useconds/step
     dir_M2 = 1;
-    CLR(PORTC, 24);   // Dir Motor2 (Forward)
+    REG_PIOC_SODR = PIO_PC24; //Pin 6 Set High (Y Motor Dir)
   }
   else
   {
-    timer_period = 2000000 / -speed;
+    timer_period = 1000000.0l / -speed; //PDB sec/step * 1,000,000 =  useconds/step
     dir_M2 = -1;
-    SET(PORTC, 24);  // DIR Motor 2
+    REG_PIOC_CODR = PIO_PC24; //Pin 6 Set Low (Run Opposite Dir)
   }
-  if (timer_period > 65535)   // Check for minimun speed (maximun period without overflow)
+  if (timer_period > ZERO_SPEED)   // Check that timer does not exceed max period
     timer_period = ZERO_SPEED;
 
-  OCR3A = timer_period;
-  // Check  if we need to reset the timer...
-  if (TCNT3 > OCR3A)
-    TCNT3 = 0;
+#if DEBUG > 0
+  Serial.print("timer7_period_control: ");
+  Serial.println(timer_period);
+#endif
+
+  Timer7.setPeriod(timer_period).start();
 }
 
